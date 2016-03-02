c7d76f2d84c612f5adb514e02d12cb321423b8e9
David Tao <David Tao>
2016-03-02 11:55:57 +0800

    Add 4.2.2 exercise

--- week-4/simple-string.rb
+++ week-4/simple-string.rb
@@ -3,7 +3,6 @@
 old_string = "Ruby is cool"
 new_string = old_string.reverse.upcase
 
-
 # RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
 
 
