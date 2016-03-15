9bc1a619d899e5290a81a9f3d5ae8a689dd86e97
David Tao <David Tao>
2016-03-15 12:31:03 +0900

    Add 6.2 and reflection

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -44,13 +44,27 @@ d.sides
 d.roll
 
 
-# Refactored Solution
-
-
-
-
-
-
-
 
 # Reflection
+=begin
+	
+What were the main differences between this die class and the last one you created in terms of implementation?
+ Did you need to change much logic to get this to work?
+
+The main logic is still there, it's just taking the rand operator to a new level.
+
+What does this exercise teach you about making code that is easily changeable or modifiable? 
+
+I think this code teaches us that code should be clear, concise, and the variables/method names should
+represent exactly what it is.
+
+What new methods did you learn when working on this challenge, if any?
+
+I didn't exactly learn any new methods in this challenge.
+
+What concepts about classes were you able to solidify in this challenge?
+
+I was able to solidify the concept of initializing my class before doing anything in order to set the
+instance variables needed.
+	
+=end
