a546a32b58a0f07dd42a7f9d7588fe9ae54c8cba
David Tao <David Tao>
2016-03-20 13:28:22 +0800

    Add 6.5, 6.7 and GPS

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -6,7 +6,7 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
 # attempts:
 # ============================================================
-
+puts array[1][1][2][0]
 
 
 # ============================================================
@@ -18,7 +18,7 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 
 # attempts:
 # ============================================================
-
+puts hash [:outer][:inner]["almost"][3]
 
 
 # ============================================================
@@ -31,7 +31,7 @@ nested_data = {array: ["array", {hash: "finished"}]}
 
 # attempts:
 # ============================================================
-
+puts nested_data [:array][1][:hash]
 
 
 # ============================================================
@@ -40,8 +40,42 @@ nested_data = {array: ["array", {hash: "finished"}]}
 
 number_array = [5, [10, 15], [20,25,30], 35]
 
+number_array.map! do |element|
+	if element.kind_of?(Array)
+		element.map! {|e| e + 5}
+	else 
+		element + 5
+	end
+end
+puts number_array
 
 
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+startup_names.map! do |element|
+	if element.kind_of?(Array)
+		element.map! {|e| if e.kind_of?(Array) 
+		e.map! {|a| a + "ly"} else e + "ly" end }
+	else 
+		element + "ly"
+	end
+end
+
+puts startup_names
+=begin
+REFLECTION
+What are some general rules you can apply to nested arrays?
+Nested arrays can be called upon through successive square brackets
+
+What are some ways you can iterate over nested arrays?
+They can be iterated through using the condition 'if the iterated element is an array...'
+
+
+Did you find any good new methods to implement or did you re-use one you 
+were already familiar with? What was it and why did you decide that was a good option?
+We reused mostly familiar statements, but also used map!, which is somewhat more foreign to
+me. We used map! because it's a destructive method.
+
+=end
