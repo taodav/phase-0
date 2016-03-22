1a99ee881f4327d60b53f4e75f4fd80f0b5447fc
David Tao <David Tao>
2016-03-22 22:01:53 +0800

    Add challenge 7.2

---
+++ week-7/eloquent.js
@@ -0,0 +1,69 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+// var addition = 5
+// addition += 4
+// console.log(addition)
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+// var triangle = ""
+// for (var counter = 0; counter < 7; counter += 1){
+// 	console.log(triangle += "#")
+// }
+
+// for (var number = 0; number <= 100; number += 1){
+// 	if(number % 3 == 0)
+// 		console.log("fizz");
+// 	else if(number % 5 == 0)
+// 		console.log("bizz");
+// 	else
+// 		console.log(number)
+// }
+
+// var grid = prompt("What's your given grid size?", "")
+// var board = ""
+// console.log(grid)
+// for (var num_of_lines = 0; num_of_lines < grid; num_of_lines += 1){
+// 	if (num_of_lines % 2 == 0)
+// 		for (var width = 0; width < grid; width += 1){
+// 			console.log(board += " #");
+// 		}
+// 	else
+// 		for (var width = 0; width < grid; width += 1){
+// 			console.log(board += "# ");
+// 		}
+
+// }
+
+// Favorite foods
+// var favorite_food = prompt("What's your favorite food?")
+// console.log("hey! " + favorite_food + " is my favorite food too!")
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+// function min(a,b) {
+// 	if (a < b)
+// 		return a;
+// 	else
+// 		return b;
+// }
+
+// console.log(min(2,3))
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {
+	name: "David"
+	age: 21
+	foods: ["Chicken", "Sweet Potato", "Sushi"]
+	quirk: "Likes to watch children's cartoons"
+}
